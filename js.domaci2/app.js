4027ffb3a8b60fefb75a4fe82e4956d4e71e0bf2
Snezana Popovic <popovicsnezana45@gmail.com>
2025-06-04 16:51:45 -0700

    Domaci(while,for)

---
+++ js.domaci2/app.js
@@ -0,0 +1,29 @@
+
+// 2. Unos brojeva – preskoči 0, prekini ako je broj veći od 100
+// while(true){
+//     let broj = +prompt("unesite broj (prekid je ako je > 100):");
+//     if(broj === 0) continue;
+//     if(broj > 100) break;
+//     console.log(`uneti broj je: ${broj}`);
+// }
+// console.log((`zavrsen unos.`));
+
+
+// 3. Ispiši samo neparne brojeve od 1 do 20
+// for(let i=0; i<21; i++){
+//     if(i % 2 === 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// 4. Saberi sve brojeve od 1 do 20, ali preskoči one koji su deljivi sa 4
+// let zbir=0;
+// for(let i=1; i<21; i++){
+//     if(i % 4 === 0){
+//         continue;
+//     }
+//     zbir +=i;
+// }
+// console.log(`zbir je ${zbir}.`);
