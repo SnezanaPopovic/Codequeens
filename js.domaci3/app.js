330d104a51759a95c86303adcaaad5fd25f8deb6
Snezana Popovic <popovicsnezana45@gmail.com>
2025-06-04 16:52:38 -0700

    Domaci(ugnjezdene petlje)

---
+++ js.domaci3/app.js
@@ -0,0 +1,26 @@
+
+// ****
+// ****
+// ****
+for(let i=0; i<3; i++){
+    let linija="";
+    for(let j=0; j<5; j++){
+        linija += "*";
+    }
+    console.log(linija);
+}
+
+
+
+// * 
+// **
+// ***
+// ****
+// *****
+// for(let i=1; i<=5; i++){
+//     let red="";
+//     for(let j=1; j<=i; j++){
+//         red +="*";
+//     }
+//     console.log(red);
+// }
