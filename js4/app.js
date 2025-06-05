18b1574480ac742bdcbc41ad9a113b2c5cbd9d5f
Snezana Popovic <popovicsnezana45@gmail.com>
2025-06-05 10:47:50 -0700

    switch,for petlja

---
+++ js4/app.js
@@ -0,0 +1,98 @@
+// switch, for petlja
+
+// let broj = Number(prompt("unesite broj:"));
+// switch(broj){
+//     case(5):
+//     console.log("uneli ste broj 5");
+//     break;
+//     case(10):
+//     console.log("uneli ste broj 10");
+//     break;
+
+//     default:
+//         console.log("niste uneli ni 5 ni 10");
+//         break;
+// }
+// if (broj == 5){
+//     console.log("first");
+// }else if(broj == 10){
+//     console.log();
+// }else{
+//     console.log(first);
+// }
+
+
+
+// let mesec = Number(prompt("unesite mesec: "));
+
+// switch(mesec){
+//     case(1):
+//         console.log("januar,31 dan");
+//         break;
+//     case(2):
+//         console.log("februar,28 dana");
+//         break;
+//     case(3):
+//         console.log("mart,31 dan");
+//         break;
+//     case(4):
+//         console.log("april,30 dana");
+//         break;
+//     case(5):
+//         console.log("maj,31 dan");
+//         break;
+// }
+
+
+// let mesec = Number(prompt("unesite mesec: "));
+
+// switch(mesec){
+//     case 1: case 3: case 5:
+//         console.log("31 dan");
+//         break;
+//     case 2:
+//         console.log("28 dana");
+//         break;
+//     case 4: case 6: case 8:
+//         console.log("31 dan");
+//         break;
+// }
+
+
+// for(let i=1; i<10; i++){
+//     console.log(i);
+// }
+
+
+// let zbir=0;
+// for(let i=1; i<=100; i++){
+//     zbir += i;
+// }
+// console.log(zbir);
+
+
+// ispoisati sve parne brojeve do 100
+// for(let i=0; i<=100; i+=2){
+//     console.log(i);
+// }
+
+// ispisati 100 parnih brojeva
+// for(let i=0; i<200; i+=2){
+//     console.log(i)
+// }
+
+// drugi nacin
+// let brojac=0;
+// for(let i =0; brojac<100; brojac++){
+//     console.log(i);
+//     i+=2;
+// }
+
+
+//napisati prvih 10 brojeva unazad
+// for(let i=10; i>1; i--){
+//     console.log(i);
+// }
+
+
+
